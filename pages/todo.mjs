bba4e1711b846f47630eeb715c17e946afeb20fa
James Ross <git@james-ross.co.uk>
2022-05-18 20:02:35 +0100

    feat: Add to do page

---
+++ pages/todo.mjs
@@ -0,0 +1,10 @@
+import { getElements } from "../modules/elements.mjs";
+
+const e = getElements();
+
+// TODO: Calculate and display required materials from characters/weapons/targets
+// TODO: Display when materials are sufficient
+// TODO: Display possible domains to enter
+// TODO: Allow editing quantity of materials
+// TODO: Allow submitting domain results (adding materials to inventory)
+// TODO: Allow submitting new character/talent/weapon levels (consuming materials from inventory)
