1779275cf09cf21bccd31b42e74a5fd0536b41e2
James Ross <git@james-ross.co.uk>
2022-05-18 21:55:09 +0100

    feat: Calculate level and ascension materials for characters and weapons (not displayed)

--- pages/todo.mjs
+++ pages/todo.mjs
@@ -1,5 +1,5 @@
 import { getElements } from "../modules/elements.mjs";
-import { getCard } from "../modules/genshin.mjs";
+import { getCard, getCharacterLevelExperience, getWeaponLevelExperience } from "../modules/genshin.mjs";
 import { KEY, load } from "../modules/storage.mjs";
 
 const e = getElements();
@@ -15,6 +15,9 @@ const characters = load("characters");
 const weapons = load("weapons");
 const materials = load("materials");
 const targets = load("targets");
+
+const required = Object.create(null);
+
 for (const target of Object.values(targets)) {
     const character = target.type === "character" ? target[KEY] : null;
     const weapon =
@@ -26,9 +29,76 @@ for (const target of Object.values(targets)) {
 
     const characterSource = characters[character];
     const characterTarget = characterSource ? target : null;
+    const characterData = GenshinDb.character(character);
+
     const weaponSource = weapons[weapon];
     const weaponTarget = weaponSource ? target : null;
+    const weaponData = GenshinDb.weapon(weapon);
 
-    if (character) e.todo.list.append(getCard(GenshinDb.character(character), { label: `${target.type[0]} ${characterSource.level} --> ${characterTarget.level}`}));
-    if (weapon) e.todo.list.append(getCard(GenshinDb.weapon(weapon), { label: `${target.type[0]} ${weaponSource.level} --> ${weaponTarget.level}`}));
+    if (character)
+        e.todo.list.append(
+            getCard(characterData, {
+                label: `${target.type[0]} ${characterSource.level} --> ${characterTarget.level}`,
+            })
+        );
+    if (weapon)
+        e.todo.list.append(
+            getCard(weaponData, {
+                label: `${target.type[0]} ${weaponSource.level} --> ${weaponTarget.level}`,
+            })
+        );
+
+    if (character) {
+        if (characterSource.level < characterTarget.level) {
+            // TODO: Special consideration is needed for wasted EXP at ascension levels
+            const { experience, mora } = getCharacterLevelExperience(characterSource.level, characterTarget.level);
+            console.log(character, "level", characterSource.level, characterTarget.level, { experience, mora });
+            required["Character EXP"] ||= 0;
+            required["Character EXP"] += experience;
+            required["Mora"] ||= 0;
+            required["Mora"] += mora;
+        }
+        for (let ascension = characterSource.ascension + 1; ascension <= characterTarget.ascension; ascension++) {
+            for (const cost of characterData.costs[`ascend${ascension}`]) {
+                console.log(character, "ascension", ascension, cost);
+                required[cost.name] ||= 0;
+                required[cost.name] += cost.count;
+            }
+        }
+        if (
+            characterSource.talent1 < characterTarget.talent1 ||
+            characterSource.talent2 < characterTarget.talent2 ||
+            characterSource.talent3 < characterTarget.talent3
+        ) {
+            // TODO: Calculate character talent level up materials
+        }
+    }
+
+    if (weapon) {
+        if (weaponSource.level < weaponTarget.level) {
+            // TODO: Special consideration is needed for wasted EXP at ascension levels
+            const { experience, mora } = getWeaponLevelExperience(
+                weaponData.rarity,
+                weaponSource.level,
+                weaponTarget.level
+            );
+            console.log(weapon, "level", weaponData.rarity, weaponSource.level, weaponTarget.level, {
+                experience,
+                mora,
+            });
+            required["Weapon EXP"] ||= 0;
+            required["Weapon EXP"] += experience;
+            required["Mora"] ||= 0;
+            required["Mora"] += mora;
+        }
+        for (let ascension = weaponSource.ascension + 1; ascension <= weaponTarget.ascension; ascension++) {
+            for (const cost of weaponData.costs[`ascend${ascension}`]) {
+                console.log(weapon, "ascension", ascension, cost);
+                required[cost.name] ||= 0;
+                required[cost.name] += cost.count;
+            }
+        }
+    }
 }
+
+console.log(required);
